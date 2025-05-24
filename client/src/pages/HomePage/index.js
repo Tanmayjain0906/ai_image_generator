08da5c3cbdcc58a3fd778b39f8aba94bba42e14e
Tanmayjain0906 <tanmayjain983@gmail.com>
2025-05-24 08:59:10 +0530

    ai image

--- client/src/pages/HomePage/index.js
+++ client/src/pages/HomePage/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './style.css';
 import Header from '../../components/Header';
 import Steps from '../../components/Steps';
 import Description from '../../components/Description';
 import Testimonials from '../../components/Testimonials';
 import SeeTheMagic from '../../components/SeeTheMagic';
+import { AppContext } from '../../context/AppContext';
+import Loader from '../../components/Loader';
 
 
 const HomePage = () => {
+
+  const {isUserLoading} = useContext(AppContext);
+
+  if(isUserLoading)
+  {
+    return (
+      <Loader/>
+    )
+  }
   
   return (
     <div className="homepage">
